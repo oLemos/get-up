a2bd9cc21af3336b10b3af73965999caddf7b656
oLemos <lemmos@outlook.com.br>
2021-04-18 23:23:15 -0300

    levelling and notifications

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/oLemos.png" alt="Gabriel Lemos" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Gabriel Lemos</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
